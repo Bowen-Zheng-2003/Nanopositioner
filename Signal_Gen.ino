e5bab10706f5f621ecf63eef854e8a7d4223dffa
Bowen-Zheng-2003 <94717310+Bowen-Zheng-2003@users.noreply.github.com>
2024-12-29 14:40:18 -0500

    Finished bang bang control

--- Signal_Gen.ino
+++ Signal_Gen.ino
@@ -1,8 +1,10 @@
-const int SIGNAL   = 7; 
-const int CURSOR   = 8; 
-const int MODE     = 9; 
-const int ADD      = 10; 
-const int SUBTRACT = 11; 
+const int SIGNAL    = 7; 
+const int CURSOR    = 8; 
+const int MODE      = 9; 
+const int ADD       = 10; 
+const int SUBTRACT  = 11;
+const int DELAY_ON  = 50; 
+const int DELAY_OFF = 100; 
 
 void setup() {
   pinMode(MODE, OUTPUT);
@@ -13,12 +15,10 @@ void setup() {
 
   Serial.begin(115200);
   calibration();
+  Serial.println("Type '0' to change cursor, '1', to increment, '2' to decrement, and '3' to output");
 }
 
 void loop() {
-  
-  Serial.println("Type '0' to change cursor, '1', to increment, '2' to decrement, and '3' to output");
-
   if (Serial.available() > 0) {
     char input = Serial.read();
     switch (input) {
@@ -34,50 +34,71 @@ void loop() {
       case '3':
         signalOutput();
         break;
+      case '4':
+        mode();
+        break;
     }
   }
+  signalOutput(); // turn it on
+  delay(5000);
+  signalOutput(); // turn it off
+  reverse();
+  signalOutput(); // turn it on
+  delay(5000);
+  signalOutput(); // turn it off
+  forward();
 }
 
 void mode() {
   digitalWrite(MODE, HIGH); // Turn on the MOSFET
-  delay(100);
+  delay(DELAY_ON);
   digitalWrite(MODE, LOW);  // Turn off the MOSFET
-  delay(1000);
+  delay(DELAY_OFF);
 }
 
 void cursor() {
   digitalWrite(CURSOR, HIGH); // Turn on the MOSFET
-  delay(100);
+  delay(DELAY_ON);
   digitalWrite(CURSOR, LOW);  // Turn off the MOSFET
-  delay(500);
+  delay(DELAY_OFF);
 }
 
 void increment() {
   digitalWrite(ADD, HIGH); // Turn on the MOSFET
-  delay(100);
+  delay(DELAY_ON);
   digitalWrite(ADD, LOW);  // Turn off the MOSFET
-  delay(500);
+  delay(DELAY_OFF);
 }
 
 void decrement() {
   digitalWrite(SUBTRACT, HIGH); // Turn on the MOSFET
-  delay(100);
+  delay(DELAY_ON);
   digitalWrite(SUBTRACT, LOW);  // Turn off the MOSFET
-  delay(500);
+  delay(DELAY_OFF);
 }
 
 void signalOutput() {
   digitalWrite(SIGNAL, HIGH); // Turn on the MOSFET
-  delay(100);
+  delay(DELAY_ON);
   digitalWrite(SIGNAL, LOW);  // Turn off the MOSFET
-  delay(500);
+  delay(DELAY_OFF);
+}
+
+void forward() {
+  for (int i = 0; i < 6; i++) {
+    mode();
+  }
+}
+
+void reverse() {
+  mode();
 }
 
 void calibration() {
-  decrement();
   for (int i = 0; i < 3; i++) {
     mode();
   }
+  decrement();
   for (int i = 0; i < 2; i++) {
     cursor();
   }
