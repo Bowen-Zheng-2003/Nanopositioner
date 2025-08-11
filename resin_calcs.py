51ffba4ff50700a4af8ed9c5739ba4e19ce59d7f
Bowen-Zheng-2003 <94717310+Bowen-Zheng-2003@users.noreply.github.com>
2025-08-10 20:21:21 -0400

    Update resin_calcs.py

--- resin_calcs.py
+++ resin_calcs.py
@@ -10,25 +10,35 @@ k_b_steel = round(bending_stiffness(E_steel, b_steel, h_steel, L_steel), 1)
 
 E_resin = 1.6e9   # Pa (Young's modulus)
 b_resin = 0.00635 # m (Thickness of the plate)
-h_resin = 0.003  # m (Height of the plate)
-L_resin = 0.02   # m (Length of the plate)
+h_resin = 0.003   # m (Height of the plate)
+L_resin = 0.02    # m (Length of the plate)
 k_b_resin = round(bending_stiffness(E_resin, b_resin, h_resin, L_resin), 1)
 
 print("Flexural stiffness of steel flexure is " + str(k_b_steel) + " N/m")
 print("Flexural stiffness of resin flexure is " + str(k_b_resin) + " N/m")
 
-## 2nd part of flexure's stiffness
-E_steel1 = 200e9   # Pa (Young's modulus)
-b_steel1 = 0.00635 # m (Thickness of the plate)
-h_steel1 = 0.004   # m (Height of the plate)
-L_steel1 = 0.0077  # m (Length of the plate)
-k_b_steel1 = round(bending_stiffness(E_steel1, b_steel1, h_steel1, L_steel1), 1)
+## Contact stiffness
+import math
+def contact_stiffness(L, v, E, F, R):
+    return (L/((2*(1-v*v)/E)*math.log((2*(1-v*v)/E)*L**3/(F*R))))
 
-E_resin1 = 1.6e9   # Pa (Young's modulus)
-b_resin1 = 0.00635 # m (Thickness of the plate)
-h_resin1 = 0.0092  # m (Height of the plate)
-L_resin1 = 0.0077    # m (Length of the plate)
-k_b_resin1 = round(bending_stiffness(E_resin1, b_resin1, h_resin1, L_resin1), 1)
+force = 1       # N
+radius_steel = 0.00075 # m
+L_s = 0.00635    # m (length of contact)
+v_s = 0.3        # Poisson's ratio
+E_s = 200e9      # Pa (Young's modulus)
+k_c_steel = round(abs(contact_stiffness(L_s, v_s, E_s, force, radius_steel)), 1)
 
-print("Flexural stiffness of steel flexure's 2nd part is " + str(k_b_steel1) + " N/m")
-print("Flexural stiffness of resin flexure's 2nd part is " + str(k_b_resin1) + " N/m")
+radius_resin = 0.0002 # m
+L_r = 0.01    # m (length of contact)
+v_r = 0.3        # Poisson's ratio
+E_r = 1.6e9      # Pa (Young's modulus)
+k_c_resin = round(abs(contact_stiffness(L_r, v_r, E_r, 10, radius_resin)), 1)
+
+print("Contact stiffness of steel on steel is " + str(k_c_steel) + " N/m")
+print("Contact stiffness of resin on resin is " + str(k_c_resin) + " N/m")
+
+
+
+
+
