2544b722fa87fb8ac4a728268f67c5e1866d9253
Bowen-Zheng-2003 <94717310+Bowen-Zheng-2003@users.noreply.github.com>
2025-11-30 18:59:37 -0500

    Strengthened code (going back to original + NOT_A & NOT_B check)

--- Tests/relative_position_posic_encoder.cpp
+++ Tests/relative_position_posic_encoder.cpp
@@ -1,102 +1,70 @@
-//// RELATIVE POSITIONING
-unsigned long previousMillis = 0;    // Stores last time Serial was printed
-const long interval = 100;           // Interval at which to print (milliseconds)
+// #define CHANNEL_A 2
+// #define CHANNEL_B 3
+// #define CHANNEL_NOT_A 18
+// #define CHANNEL_NOT_B 19
+#define CHANNEL_A A2
+#define CHANNEL_B A3
+#define CHANNEL_NOT_A A4
+#define CHANNEL_NOT_B A5
 
-volatile long tracker = 0;
 volatile long encoder_counts = 0;
-float counts_to_micron = 5;
+volatile unsigned long isr_start_time = 0;
+volatile unsigned long isr_end_time = 0;
+volatile unsigned long last_isr_duration = 0;
+volatile int xEncoderStatus  = 0;      // [binary] Past and Current A&B values of the encoder  (Declared 'volatile', since it is updated in a function called by interrupts)
 
 void setup() {
-  DDRE = 0b00000000; // For pins 2 & 3
-  // set up external interrupt registers 
-  EICRA = 0;
-  EICRB = 0b00000101 ; // 0101 indicates that any change (rising/falling) on interrupts 4 and 5 will signal change (now counting in quad)
-  EIMSK = 0b00110000;
-  EIFR = 0b00110000; // For pins 18 & 19
-
-  // setting up reading the complementary values (NOT A & NOT B)
-  DDRD = 0b00000000;
-
-
   Serial.begin(115200);
-  
+
+  pinMode(CHANNEL_A, INPUT);
+  pinMode(CHANNEL_B, INPUT);
+  pinMode(CHANNEL_NOT_A, INPUT);
+  pinMode(CHANNEL_NOT_B, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(CHANNEL_A), updateXPosition, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CHANNEL_B), updateXPosition, CHANGE);
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;   // Save the last time you printed
-    // Serial.print("Encoder counts: ");
-    Serial.println(encoder_counts);   // Print the encoder counts
-    // Serial.print("Reference counts: ");
-    // Serial.println(tracker);   // Print the encoder counts
-  }
+  Serial.print("Encoder counts: ");
+  Serial.print(encoder_counts*0.01953);
+  Serial.print("  Last ISR duration (us): ");
+  Serial.println(last_isr_duration);
+  delay(100);
 }
 
-ISR(INT4_vect){ // INT4 is pin 2, port E (Channel A)
-  if ((PINE & 0b00010000) == 0b00010000){ // checks if CHANNEL A is rising edge
-    if((PIND & 0b00001000) == 0b00001000){ // checking pin 18, which is NOT A
-      // encoder_counts = encoder_counts + 0; // if they're both high, there's an error
-    }
-    else if((PIND & 0b00001000) == 0b00000000){
-      // encoder_counts = encoder_counts + 1; // if they're opposite, there's no error
-
-      if((PINE & 0b00100000) == 0b00100000){
-        encoder_counts = encoder_counts - 1;
-      }
-      else if((PINE & 0b00100000) == 0b00000000){
-        encoder_counts = encoder_counts + 1;
-      }
-    }
+void updateXPosition() {
+  isr_start_time = micros();
+  // Bitwise shift left by one bit, to make room for a bit of new data:
+  xEncoderStatus <<= 1;   
+  // Use a compound bitwise OR operator (|=) to read the A channel of the encoder (pin 2)
+  // and put that value into the rightmost bit of encoderStatus:
+  if (digitalRead(CHANNEL_A) != digitalRead(CHANNEL_NOT_A)){
+    xEncoderStatus |= digitalRead(CHANNEL_A);   
   }
-  else if ((PINE & 0b00010000) == 0b00000000){ // checks if CHANNEL A is falling edge
-    if((PIND & 0b00001000) == 0b00001000){
-      // encoder_counts = encoder_counts + 1;
-
-      if((PINE & 0b00100000) == 0b00100000){
-        encoder_counts = encoder_counts + 1;
-      }
-      else if((PINE & 0b00100000) == 0b00000000){
-        encoder_counts = encoder_counts - 1;
-      }
-    }
-    else if((PIND & 0b00001000) == 0b00000000){
-      // encoder_counts = encoder_counts + 0;
-    }
+  // Bitwise shift left by one bit, to make room for a bit of new data:
+  xEncoderStatus <<= 1;
+  // Use a compound bitwise OR operator  (|=) to read the B channel of the encoder (pin 3)
+  // and put that value into the rightmost bit of encoderStatus:
+  if (digitalRead(CHANNEL_B) != digitalRead(CHANNEL_NOT_B)){
+    xEncoderStatus |= digitalRead(CHANNEL_B);   
   }
-  // tracker ++;
-}
-
-ISR(INT5_vect){ // INT5 is pin 3, port E (Channel B)
-  if ((PINE & 0b00100000) == 0b00100000){ // checks if CHANNEL B is rising edge
-    if((PIND & 0b00000100) == 0b00000100){ // checking pin 19, which is NOT B
-      // encoder_counts = encoder_counts + 0; // if they're both high, there's an error
-    }
-    else if((PIND & 0b00000100) == 0b00000000){
-      // encoder_counts = encoder_counts + 1; // if they're opposite, there's no error
-
-      if((PINE & 0b00010000) == 0b00010000){
-        encoder_counts = encoder_counts + 1;
-      }
-      else if((PINE & 0b00010000) == 0b00000000){
-        encoder_counts = encoder_counts - 1;
-      }
-    }
+  // encoderStatus is truncated to only contain the rightmost 4 bits by  using a 
+  // bitwise AND operator on mstatus and 15(=1111):
+  xEncoderStatus &= 15;
+  if (xEncoderStatus==2 || xEncoderStatus==4 || xEncoderStatus==11 || xEncoderStatus==13) {
+    // the encoder status matches a bit pattern that requires counting up by one
+    encoder_counts ++;         // increase the encoder count by one
+    // Serial.print("Positive ");
+    // Serial.println(xEncoderStatus);
+  } 
+  else if (xEncoderStatus==1 || xEncoderStatus==7 || xEncoderStatus==8 || xEncoderStatus==14) {
+    // the encoder status does not match a bit pattern that requires counting up by one.  
+    // Since this function is only called if something has changed, we have to count downwards
+    encoder_counts --;         // decrease the encoder count by one
+    // Serial.print("Negative ");
+    // Serial.println(xEncoderStatus);
   }
-  else if ((PINE & 0b00100000) == 0b00000000){ // checks if CHANNEL B is falling edge
-    if((PIND & 0b00000100) == 0b00000100){
-      // encoder_counts = encoder_counts + 1;
-    }
-    else if((PIND & 0b00000100) == 0b00000000){
-      // encoder_counts = encoder_counts + 0;
-      if((PINE & 0b00010000) == 0b00010000){
-        encoder_counts = encoder_counts - 1;
-      }
-      else if((PINE & 0b00010000) == 0b00000000){
-        encoder_counts = encoder_counts + 1;
-      }
-    }
-  }
-  // tracker ++;
+  isr_end_time = micros();
+  last_isr_duration = isr_end_time - isr_start_time;
 }
