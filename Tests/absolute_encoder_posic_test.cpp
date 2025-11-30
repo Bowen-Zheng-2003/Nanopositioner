41d174117d04770f9b3c5f8b0490214ec952c206
Bowen-Zheng-2003 <94717310+Bowen-Zheng-2003@users.noreply.github.com>
2025-11-29 23:32:29 -0500

    Corrected for arduino mega

--- Tests/absolute_encoder_posic_test.cpp
+++ Tests/absolute_encoder_posic_test.cpp
@@ -1,22 +1,29 @@
-const int delay_um = 2; // microseconds
+const int delay_um = 1; // microseconds
 
-const byte CLK_HIGH = B00000100; //Clock -> pin 2
-const byte CLK_LOW  = B00000000; //Clock -> pin 2
-const byte DATA = B00001000; //Data -> pin 3
+// const byte CLK_HIGH = 0b00010000; //Clock -> pin 2
+// const byte CLK_LOW  = 0b00000000; //Clock -> pin 2
+// const byte DATA = 0b00001000; //Data -> pin 3
 
 void setup() {
-  DDRD = DDRD | B00001100; // 0 is input, 1 is output, so I'm setting pin 2 as output and pin 3 as input
+  DDRE = DDRE | 0b00010000; // 0 is input, 1 is output, so I'm setting pin 2 as output and pin 3 as input
 
   Serial.begin(115200);
   delay(200); //Minimum delay after powering encoder
   //Initialize CLK high
-  PORTD = CLK_HIGH;
+  PORTE = 0b00010000;
 }
 
 void loop() {
+  // PORTE = 0b00000000;
+  // delay(100);
+  // PORTE = 0b00010000;
+  // delay(100);
+  // Serial.println((PINE & 0b00100000) == 0b00100000);
+  // delay(10);
+
   unsigned long enc_data = takeReading();
-  // Serial.println(enc_data);
-  // delay(500);
+  Serial.println(enc_data, BIN);
+  delay(500);
   // Serial.println(encoder_counts*0.078125);
 }
 
@@ -25,17 +32,16 @@ unsigned long takeReading() {
   unsigned long trash = 0;
   unsigned long bit = 0;
 
-  PORTD = CLK_LOW;
+  PORTE = 0b00000000;
   delayMicroseconds(delay_um);
 
   for(int i=0; i<31; i++){
-    PORTD = CLK_HIGH;
-    delayMicroseconds(delay_um);
+    PORTE = 0b00010000;
 
     //Store encoder position bits
-    bit = digitalRead(3);
+    bit = (PINE & 0b00100000) == 0b00100000;
 
-    PORTD = CLK_LOW;
+    PORTE = 0b00000000;
     delayMicroseconds(delay_um);
     
 
@@ -47,10 +53,10 @@ unsigned long takeReading() {
     // data = (PIND & 0b00001000) ? 1 : 0;  // Mask isolates bit 3 (1<<3), non-zero means HIGH
 
     // Serial.print("Data: ");
-    Serial.print(bit);
+    // Serial.print(bit);
   }
 
-  Serial.println(" ");
+  // Serial.println(" ");
   // Serial.print("Data: ");
   // Serial.println(data);
   delayMicroseconds(30);
